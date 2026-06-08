00eabed2a8d63aef736f84ff01aaa3b8c9dabc30
ryanumlas <ryanumlas15@gmail.com>
2026-06-08 16:18:44 +0800

    Added Cursor, Background, Customer Avatar, and Ingredient Assets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-
+import background1 from './assets/background1.png'
+import happy_bunny from './assets/bunny/happy_bunny.png'
 
 function App() {
-
   return (
-    <>
-    </>
+    <div
+      className="min-h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${background1})`,
+      }}
+    >
+    </div>
   )
 }
 
